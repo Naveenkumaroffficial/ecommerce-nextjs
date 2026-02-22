6942f4af3fd96a3ce32ee8b1dfd1b6363f593b11
user.name "Naveen <user.emai "naveenkumarofficial32@gmail.com>
2026-02-22 17:59:10 +0530

    "connection eastablish'

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page
+      <Button>shadcn</Button>/
+    </div>
+    
+  )
+}
+
+export default page
